e700dcb3128a212cb4366fd1cc62532d7bd0be32
ernebronk <ernebronkhorst@gmail.com>
2015-06-04 17:52:27 +0200

    Edits
    <3

--- main.m
+++ main.m
@@ -15,14 +15,12 @@ car = [ 2 5 ];
 carp = [ 4 3 ];
 tic
 
-name = '';
 
+name = '';
 for n = 1:6
     x = t(n);
     name = [name '_' num2str(x)];
 end
-
-
 name = ['logs/' name '.mat'];
 save(name);
 
