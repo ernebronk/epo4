9d42a97e0d40ee1a6273a319fe82bb738f9ef293
ernebronk <ernebronkhorst@gmail.com>
2015-06-04 17:40:42 +0200

    Added store function of workspaces
    Added store function of workspaces

--- main.m
+++ main.m
@@ -7,9 +7,23 @@
 % Start the program
 %    result =  program();
 
+format shortg
+t = clock;
+
 target = [ 0 3 ];
 car = [ 2 5 ];
 carp = [ 4 3 ];
 tic
 
-program;
+name = '';
+
+for n = 1:6
+    x = t(n);
+    name = [name '_' num2str(x)];
+end
+
+
+name = ['logs/' name '.mat'];
+save(name);
+
+%program;
